8627c301d25788a19cd3cb3cfa3a8f33d09bf2c1
Robin Borjesson <robin.b@outlook.com>
2019-06-16 23:26:08 +0200

    Re-add entire directory

---
+++ README.md
@@ -0,0 +1,3 @@
+# Digit Predictor
+
+Write a number in the input field and see if the neural network can identify which digit you were trying to type.
---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <div className="text-center">
+        <h1 className="bg-purple-600 m-6 p-6 rounded shadow-lg text-lg">
+          Hello App!
+        </h1>
+        <div className="text-base">Some intro text</div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
