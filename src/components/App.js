5657034bdc9a132a5d552a71d78d81582278cd92
Rinbo <robin.b@outlook.com>
2019-06-17 19:49:01 +0200

    Add canvas.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import React from "react";
+import Canvas from "./Canvas";
 
 const App = () => {
   return (
-    <div>
+    <div className="container mx-auto">
       <div className="text-center">
         <h1 className="bg-purple-600 m-6 p-6 rounded shadow-lg text-lg">
-          Hello App!
+          Predict the digit!
         </h1>
-        <div className="text-base">Some intro text</div>
+        <div className="text-base">
+          Write a single digit in the box and see if the neural network can
+          predict which number you drew.
+        </div>
+        <Canvas />
       </div>
     </div>
   );
---
+++ src/components/Canvas.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+
+const Canvas = () => {
+  
+
+  return <canvas ref="digit-canvas" />;
+};
+
+export default Canvas;
