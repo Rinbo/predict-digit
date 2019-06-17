513b09889a894b38cafa7b4d244bc0d8779795f3
Rinbo <robin.b@outlook.com>
2019-06-17 21:36:50 +0200

    Finish up canvas with offset function

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,40 +1,62 @@
 import React, { useState, useRef, useEffect } from "react";
 
 const Canvas = () => {
-  const [canvasSize] = useState({ width: 200, height: 200 });
-  const [ctx, setCtx] = useState(null);
+  const [canvasSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight
+  });
   const [painting, setPainting] = useState(false);
   const digitCanvas = useRef(null);
+  const ctx = useRef(null);
 
   useEffect(() => {
-    digitCanvas.current.width = canvasSize.width;
-    digitCanvas.current.height = canvasSize.height;
+    digitCanvas.current.width = 200;
+    digitCanvas.current.height = 200;
     digitCanvas.current.style.border = "1 px solid black";
-    setCtx(digitCanvas.current.getContext("2d"));
-    console.log(digitCanvas.current.getContext("2d"));
+    ctx.current = digitCanvas.current.getContext("2d");
   }, [canvasSize]);
 
-  const startPosition = () => {
+  const startPosition = e => {
+    console.log("start");
     setPainting(true);
+    const pos = getMousePos(e);
+    ctx.current.lineTo(pos.x, pos.y);
+    ctx.current.stroke();
   };
 
   const finishedPosition = () => {
+    console.log("finish");
     setPainting(false);
+    ctx.current.beginPath();
+  };
+
+  const getMousePos = e => {
+    const rect = digitCanvas.current.getBoundingClientRect();
+    return {
+      x: e.clientX - rect.left,
+      y: e.clientY - rect.top
+    };
   };
 
   const draw = e => {
     if (!painting) return;
-    ctx.lineWidth = 10;
-    ctx.lineCap = "round";
-    ctx.lineTo(e.clientX, e.clientY);
-    ctx.stroke();
+    console.log("drawing...");
+    const pos = getMousePos(e);
+    ctx.current.lineWidth = 10;
+    ctx.current.lineCap = "round";
+    ctx.current.strokeStyle = "black";
+
+    ctx.current.lineTo(pos.x, pos.y);
+    ctx.current.stroke();
+    ctx.current.beginPath();
+    ctx.current.moveTo(pos.x, pos.y);
   };
 
   return (
     <canvas
       ref={digitCanvas}
-      onMouseDown={startPosition}
-      onMouseUp={finishedPosition}
+      onMouseDown={e => startPosition(e)}
+      onMouseUp={() => finishedPosition()}
       onMouseMove={e => draw(e)}
     />
   );
