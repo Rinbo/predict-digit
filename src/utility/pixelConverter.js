7b773cab4f7078194e03b5bcbab5dd15c39883ad
Rinbo <robin.b@outlook.com>
2019-06-22 18:31:15 +0200

    Do some refactoring

--- src/utility/pixelConverter.js
+++ src/utility/pixelConverter.js
@@ -1,4 +1,4 @@
-import { ORIGIN_PIXEL_COUNT, CONVERTED_PIXEL_COUNT } from "./pixelConstants";
+import { SCALE_FACTOR, CONVERTED_PIXEL_COUNT } from "./pixelConstants";
 
 export const convertPixels = image => {
   const grayScaleArray = image.data.filter((e, index) => (index + 1) % 4 === 0);
@@ -22,42 +22,24 @@ export const convertPixels = image => {
 
 const scaler = imageArray => {
   const normalizedArray = imageArray.map(e => Math.ceil(e / 255));
-  /*
-  const rowByRow = [];
-  for (let i = 0; i < 168; i++) {
-    rowByRow[i] = [];
-    for (let j = 0; j < 28; j++) {
-      for (let k = j * 6; k < j * 6 + 6; k++) {
-        rowByRow[i].push(normalizedArray[k].splice(0, 6));
-      }
-    }
-  }
-  */
-
   const matrix = [];
   for (let i = 0; i < CONVERTED_PIXEL_COUNT ** 2; i++) {
     matrix[i] = [];
   }
 
   for (let i = 0; i < CONVERTED_PIXEL_COUNT; i++) {
-    for (let j = 0; j < 6; j++) {
+    for (let j = 0; j < SCALE_FACTOR; j++) {
       for (let k = 0; k < CONVERTED_PIXEL_COUNT; k++) {
-        const index = i * 28 + k;
-        matrix[index] = matrix[index].concat(normalizedArray.splice(0, 6));
+        const index = i * CONVERTED_PIXEL_COUNT + k;
+        matrix[index] += accumulate(normalizedArray.splice(0, SCALE_FACTOR));
+        
       }
     }
   }
-  const output = matrix.map(e => {
-    const pixelValue = e.reduce(
-      (accumulator, currentValue) => accumulator + currentValue
-    );
-    if (pixelValue > 18) return 255;
-    return 0;
-  });
-  return output;
-  /* 
-  const matrix = [];
-  for (let i = 0; i < CONVERTED_PIXEL_COUNT; i++) {
-    matrix[i].push(rowByRow[j].splice(0, 6));
-  } */
+
+  return matrix;
+};
+
+const accumulate = arr => {
+  return arr.reduce((accumulator, currentValue) => accumulator + currentValue);
 };
