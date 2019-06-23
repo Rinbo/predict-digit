037ccfb9613db220df8d3d7395e1a1307543ad0e
Rinbo <robin.b@outlook.com>
2019-06-23 13:57:46 +0200

    Begin a more effecient scaler algorithm

--- src/utility/pixelConverter.js
+++ src/utility/pixelConverter.js
@@ -22,7 +22,48 @@ export const convertPixels = image => {
 
 const scaler = imageArray => {
   const normalizedArray = imageArray.map(e => Math.ceil(e / 255));
-  const matrix = [];
+  const sixes = reduceArray(normalizedArray, 6);
+  const matrixRows = createMatrix(sixes, 28);
+  const matrix = createMatrix(matrixRows, 6);
+  const output = matrix.map(e => {
+    const twentyEight = [];
+    for (let i = 0; i < 28; i++) {
+      for (let j = 0; j < e.length; j++) {
+        twentyEight[i] = [].concat(twentyEight[i] || [], e[j].splice(0, 1));
+      }
+    }
+    return twentyEight;
+  });
+  debugger;
+  /* const output = matrix.map(e => {
+    const pixelValue = e.reduce(
+      (accumulator, currentValue) => accumulator + currentValue
+    );
+    if (pixelValue > 18) return 255;
+    return 0;
+  });
+  return output; */
+};
+
+const reduceArray = (arr, chunkSize) => {
+  return arr.reduce((all, currentValue, index) => {
+    const chunk = Math.floor(index / chunkSize);
+    all[chunk] = [].concat(all[chunk] || [], currentValue);
+    return all;
+  }, []);
+};
+
+const createMatrix = (arr, chunkSize) => {
+  return arr.reduce((all, currentValue, index) => {
+    const chunk = Math.floor(index / chunkSize);
+    if (!all[chunk]) all[chunk] = [];
+    all[chunk].push(currentValue);
+    return all;
+  }, []);
+};
+
+/* 
+const matrix = [];
   for (let i = 0; i < CONVERTED_PIXEL_COUNT ** 2; i++) {
     matrix[i] = [];
   }
@@ -31,15 +72,10 @@ const scaler = imageArray => {
     for (let j = 0; j < SCALE_FACTOR; j++) {
       for (let k = 0; k < CONVERTED_PIXEL_COUNT; k++) {
         const index = i * CONVERTED_PIXEL_COUNT + k;
-        matrix[index] += accumulate(normalizedArray.splice(0, SCALE_FACTOR));
-        
+        matrix[index] = matrix[index].concat(
+          normalizedArray.splice(0, SCALE_FACTOR)
+        );
       }
     }
   }
-
-  return matrix;
-};
-
-const accumulate = arr => {
-  return arr.reduce((accumulator, currentValue) => accumulator + currentValue);
-};
+*/
