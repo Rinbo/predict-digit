5d00bafc264abc7ab1642f12b0d16ca34a3f799f
Rinbo <robin.b@outlook.com>
2019-06-25 20:59:16 +0200

    Convert csv file into a matrix

--- src/utility/ForwardPropagation.js
+++ src/utility/ForwardPropagation.js
@@ -3,14 +3,28 @@ const file1 = require("../resources/Theta1.txt");
 export const forwardPropagation = X => {
   //Add bias term to X
   X.unshift(1);
+  const T1 = getFile(file1);
 
-  fetch(file1)
-    .then(response => response.text())
-    .then(text => {
-      debugger;
-      console.log(text);
-    });
-
+  console.log(T1);
   X.shift();
   return X;
 };
+
+const getFile = async input => {
+  const response = await fetch(input);
+  const text = await response.text();
+  const trimmedString = text.trim();
+  const outerArray = trimmedString.split(/\n/);
+  const output = outerArray.map(row => row.split(","));
+  return output;
+};
+
+/* 
+fetch(file1)
+    .then(response => response.text())
+    .then(text => {
+      const trimmedString = text.trim();
+      const outerArray = trimmedString.split(/\n/);
+      const output = outerArray.map(row => row.split(","));
+      T1 = output;
+    }); */
